d4fc2a05e11f0941b7e5278e9e25c29bdfed8250
weifenglin1 <junxiou1@qq.com>
2018-05-16 16:48:20 +0800

    5.16

--- zuoye/src/zuoye/Coustomer.java
+++ zuoye/src/zuoye/Coustomer.java
@@ -4,17 +4,17 @@ public class Coustomer {
 	private String name;
 	
 	
-   public String getName() {
-		return name;
-	}
+	   public String getName() {
+			return name;
+		}
 
 
-	public void setName(String name) {
-		this.name = name;
-	}
+		public void setName(String name) {
+			this.name = name;
+		}
 
 
-public void service() {
-	   System.out.println("服务信息:"+"\n"+name+"在服务中....");
-   }
+	public void service() {
+		   System.out.println("服务信息:"+"\n"+name+"在服务中....");
+	   }
 }
--- zuoye/src/zuoye/VIPCoustomer.java
+++ zuoye/src/zuoye/VIPCoustomer.java
@@ -7,15 +7,14 @@ public class VIPCoustomer extends Coustomer{
 		return level;
 	}
 
-    public  void setLevel(int level) {
+   public  void setLevel(int level) {
 		this.level = level;
 	}
 
-   
+  
 
-    public void touSu() {
-    	super.getName();
+   public void touSu() {
+   	super.getName();
 		System.out.print("投诉信息："+"\n"+"VIP"+level+"级别"+getName()+"投诉中....");
 	}
-
 }
